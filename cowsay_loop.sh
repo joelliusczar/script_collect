d667f7536d150263a4c0aa2bf2bf31b4b877ab63
Joel Pridgen <joelliuscaesar@gmail.com>
2018-01-05 11:52:47 -0500

    Update cowsay_loop.sh

--- cowsay_loop.sh
+++ cowsay_loop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#this loops through all the cowsay files, prints them to the console, and prompts the user if the cow should be kept.
 clear
 for fileItem in /usr/share/cows/* ;do
 	cowItem=$(echo $(basename "$fileItem") | sed 's/\([a-zA-Z\-]\+\)\.cow/\1/')
